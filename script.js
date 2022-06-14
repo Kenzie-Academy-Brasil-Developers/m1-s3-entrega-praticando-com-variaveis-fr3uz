c9fe20ac840f29c1f74717c8778bfc008f142d09
freuz <froisvictor@gmail.com>
2022-06-14 16:07:07 -0300

    praticando com variaveis

---
+++ script.js
@@ -0,0 +1,6 @@
+let nome = "Jhon"
+let sobrenome = "Doyle"
+let anoNascimento = (2022-1991)
+let cidade = "Curitiba"
+
+console.log("Meu nome é "+nome+" "+sobrenome+", tenho "+anoNascimento+" anos e moro em "+cidade)
